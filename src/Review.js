1a5bdf0cf230e1eef6220500b90913f9f0346f83
Aravind Reddy P <ponugotiaravind@gmail.com>
2021-04-11 16:27:37 -0400

    initial commit

---
+++ src/Review.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currIndex) => currIndex + 1);
+  };
+
+  const prevPerson = () => {
+    setIndex((currIndex) => currIndex - 1);
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name} </h4>
+      <h4 className="job">{job} </h4>
+      <h4 className="info">{text} </h4>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Review;
